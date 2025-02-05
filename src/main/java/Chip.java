9ad60e9667b3b2a1f180d58ac8e06dbf70e34cd7
vihaan27 <vihaan.asthana@gmail.com>
2025-02-05 21:46:22 +0800

    Add greeting and exit messages.

---
+++ src/main/java/Chip.java
@@ -0,0 +1,9 @@
+public class Chip {
+    public static void main(String[] args) {
+        String greeting = "Hello! I'm Duke. \nWhat can I do for you?";
+        String exit = "Bye. Hope to see you again soon!";
+
+        System.out.println(greeting);
+        System.out.println(exit);
+    }
+}
