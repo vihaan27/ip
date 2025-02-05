a6062212c3f32e2eaeb87d4b16fc352fda893d99
vihaan27 <vihaan.asthana@gmail.com>
2025-02-06 00:31:37 +0800

    Add functionality to 'echo' user commands.

--- src/main/java/Chip.java
+++ src/main/java/Chip.java
@@ -1,9 +1,20 @@
+import java.util.Scanner;
 public class Chip {
     public static void main(String[] args) {
-        String greeting = "Hello! I'm Duke. \nWhat can I do for you?";
+        String greeting = "Hello! I'm Chip. What can I do for you?";
         String exit = "Bye. Hope to see you again soon!";
-
         System.out.println(greeting);
+
+        Scanner in = new Scanner(System.in);
+        String line;
+        do {
+            System.out.print("~ ");
+            line = in.nextLine();
+            if (!line.equals("bye")) {
+                System.out.println(line);
+            }
+        } while(!line.equals("bye"));
+        
         System.out.println(exit);
     }
 }
