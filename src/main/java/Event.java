aab0ea4ad6a1c2448d9f95d4a6eb5891c814d050
vihaan27 <vihaan.asthana@gmail.com>
2025-02-14 15:38:52 +0800

    Reformat to improve code quality.

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,8 +1,8 @@
-public class Event extends Task{
+public class Event extends Task {
     protected String startTime;
     protected String endTime;
 
-    public Event(String description, String startTime, String endTime){
+    public Event(String description, String startTime, String endTime) {
         super(description);
         this.startTime = startTime;
         this.endTime = endTime;
